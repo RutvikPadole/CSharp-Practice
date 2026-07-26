60658d766a99a72d6e98bb7cdd22ce9d15b835bc
RutvikPadole <rutvikpadole23@gmail.com>
2026-07-26 11:56:18 +0530

    Check the number is positive or negative

---
+++ C#_Practice/NumberCheck.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace C__Practice
+{
+    internal class NumberCheck
+    {
+        static void Main(string[] args)
+        {
+            // Take a number and check positive / negative
+
+            Console.WriteLine("Enter number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num > 0)
+                Console.WriteLine("Positive number");
+            else
+                Console.WriteLine("Negative number");
+        }
+    }
+}
