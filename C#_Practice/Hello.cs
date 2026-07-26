2b7ecaf0fd9bfc4d5499a930319b5d3a7a5da5dc
RutvikPadole <rutvikpadole23@gmail.com>
2026-07-26 11:31:04 +0530

    First Program

---
+++ C#_Practice/Hello.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Channels;
+
+namespace C__Practice
+{
+    internal class Hello
+    {
+       static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World !");
+        }
+    }
+}
