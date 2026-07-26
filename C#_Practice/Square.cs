804d184882146d0400e3c9e9da143bd1cf5b7ff1
RutvikPadole <rutvikpadole23@gmail.com>
2026-07-26 11:39:59 +0530

    Square of number

---
+++ C#_Practice/Square.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Practice
+{
+    internal class Square
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter one number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(num * num);
+        }
+    }
+}
