e6d54db21f7537e98d9793388e5892417e6f3512
RutvikPadole <rutvikpadole23@gmail.com>
2026-07-27 22:51:30 +0530

    Task Questions

---
+++ C#_Practice/CountDigit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Practice
+{
+    internal class CountDigit
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int count = 0;
+
+            while (num > 0)
+            {
+                 count++;
+
+                num = num / 10;
+
+               
+            }
+            Console.WriteLine("The number of digit is: " + count);
+        }
+    }
+}
+
+             
