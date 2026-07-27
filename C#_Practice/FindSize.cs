da24d7b427a9af11f262cdbcdbd4988445f798d7
RutvikPadole <rutvikpadole23@gmail.com>
2026-07-27 22:52:22 +0530

    Add palindrome task example

---
+++ C#_Practice/FindSize.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Practice
+{
+    internal class FindSize
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
