e195b3c445f29e1bca77ff5616a6ba9c10fed67e
RutvikPadole <rutvikpadole23@gmail.com>
2026-08-02 19:38:54 +0530

    complete todays task

---
+++ C#_Practice/MonthSeason.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Practice
+{
+    internal class MonthSeason
+    {
+        static void Main()
+        {
+            // Month to season: Take a month number (1-12) and print the season
+            // using switch-case with grouped cases.
+
+            Console.WriteLine("Enter month number (1-12)");
+            int month = Convert.ToInt32(Console.ReadLine());
+
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Winter");
+                    break;
+
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("Spring");
+                    break;
+
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Summer");
+                    break;
+
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Autumn");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid month number");
+                    break;
+            }
+        }
+    }
+}
---
+++ C#_Practice/Switch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime;
+using System.Text;
+
+namespace C__Practice
+{
+    internal class Switch
+    {
+        static void Main(string[] args)
+        {
+            // Take a day number and print only "Weekday" or "Weekend" — using case grouping
+
+            Console.WriteLine("Enter day number (1-7)");
+            int day = Convert.ToInt32(Console.ReadLine());
+
+            switch(day)
+            {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("Weekdays");
+                    break;
+
+                case 6:
+                case 7: 
+                    Console.WriteLine("Weekend days");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid day number");
+                    break;
+
+            }
+            
+        }
+    }
+}
