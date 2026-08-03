611c0863f4e8a43beb3a85daae8cd330b16b4a53
RutvikPadole <rutvikpadole23@gmail.com>
2026-08-03 10:37:09 +0530

    complete ternary operator task

---
+++ C#_Practice/ChechEvenTernary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+
+namespace C__Practice
+{
+    internal class ChechEvenTernary
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            string result;
+
+            result = num % 2 == 0 ? "Even" : "Odd";
+
+            Console.WriteLine(result);
+        }
+          
+
+    }
+}
