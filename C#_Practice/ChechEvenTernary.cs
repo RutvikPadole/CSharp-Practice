15484c2a93c86733e68ec8bd0ab2c67ad67051ae
RutvikPadole <rutvikpadole23@gmail.com>
2026-08-03 10:50:33 +0530

    correction in one program

--- C#_Practice/ChechEvenTernary.cs
+++ C#_Practice/ChechEvenTernary.cs
@@ -5,10 +5,12 @@ using System.Text;
 
 namespace C__Practice
 {
-    internal class ChechEvenTernary
+    internal class CheckEvenTernary
     {
         static void Main()
         {
+            // Check if a number is even or odd using ternary operator (num % 2 == 0 ? "Even" : "Odd").
+
             Console.WriteLine("Enter number");
             int num = Convert.ToInt32(Console.ReadLine());
 
