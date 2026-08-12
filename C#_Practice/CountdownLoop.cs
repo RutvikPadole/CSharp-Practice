3c2ea382f93baffb1f4e6fa571ecc2aeeee1cfd0
RutvikPadole <rutvikpadole23@gmail.com>
2026-08-12 23:26:27 +0530

    Complete task

---
+++ C#_Practice/CountdownLoop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Practice
+{
+    internal class CountdownLoop
+    {
+        static void Main(string[] args)
+        {
+            for(int i=10;  i>=1; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
