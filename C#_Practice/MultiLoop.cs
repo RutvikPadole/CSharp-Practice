a6b64ea47d10c07082e6c3273f4d4ef11d1322dd
RutvikPadole <rutvikpadole23@gmail.com>
2026-08-13 00:40:00 +0530

    make a changes

--- C#_Practice/MultiLoop.cs
+++ C#_Practice/MultiLoop.cs
@@ -14,7 +14,6 @@ namespace C__Practice
             Console.WriteLine("Enter number");
             int num = Convert.ToInt32(Console.ReadLine());
 
-
             for (int i = 1; i <= 10; i++)
             {
                 Console.WriteLine(num + "X" + i +" = " + (num*i));
