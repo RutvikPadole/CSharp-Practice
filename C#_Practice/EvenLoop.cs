4e7907b5be789cb0c760d7d8dc1ce6c7950805bc
RutvikPadole <rutvikpadole23@gmail.com>
2026-08-12 23:49:34 +0530

    Check even number using for loop

---
+++ C#_Practice/EvenLoop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace C__Practice
+{
+    internal class EvenLoop
+    {
+        // Print all even numbers between 1 and 20.
+
+        static void Main(string[] args)
+        {
+            for(int i=2; i<=20; i=i+2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
