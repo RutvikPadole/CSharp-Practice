4af1513841cbbd8c531e55434099c95bcf5fb7fc
RutvikPadole <rutvikpadole23@gmail.com>
2026-08-13 00:12:26 +0530

    calculate sum of numbers using for loop

---
+++ C#_Practice/SumLoop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Practice
+{
+    internal class SumLoop
+    {
+        // Calculate the sum of numbers from 1 to 100
+        // (don't print each number, just print the final sum).
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            for(int i=1;i<=100;i++)
+            {
+                sum = sum + i;   
+            }
+
+            Console.WriteLine(sum);
+        }
+
+    }
+}
