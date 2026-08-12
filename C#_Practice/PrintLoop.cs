7a8979cfc738e4ca02edf22ab4c31037a2b89308
RutvikPadole <rutvikpadole23@gmail.com>
2026-08-12 23:02:25 +0530

    Start For Loops Program

---
+++ C#_Practice/PrintLoop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Practice
+{
+    internal class PrintLoop
+    {
+        static void Main()
+        {
+            for(int i=1; i<=10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
