04a36e8738f43836f996991b149e9342c4f87d72
RutvikPadole <rutvikpadole23@gmail.com>
2026-08-13 20:09:39 +0530

    complete the task

---
+++ C#_Practice/DivisibleNumForLoop.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Practice
+{
+    internal class DivisibleNumForLoop
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+
+            for (int i = 1; i <= 50; i++)
+            {
+                if (i % 7 == 0)
+                {
+                    count = count + 1;
+                }
+               
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
