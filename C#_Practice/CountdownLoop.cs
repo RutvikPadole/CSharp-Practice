78aeaf32fa31da26931806ff948ab633dd30a5fb
RutvikPadole <rutvikpadole23@gmail.com>
2026-08-19 21:09:30 +0530

    Making changes in program

--- C#_Practice/CountdownLoop.cs
+++ C#_Practice/CountdownLoop.cs
@@ -7,7 +7,6 @@ namespace C__Practice
     internal class CountdownLoop
     {
         //Print numbers from 10 down to 1 (countdown).
-
         static void Main(string[] args)
         {
             for(int i=10;  i>=1; i--)
