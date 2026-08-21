094612eeb22095521787ae3e3ae4f9ebdf9c7873
RutvikPadole <rutvikpadole23@gmail.com>
2026-08-21 20:27:21 +0530

    complete the task

---
+++ C#_Practice/Linq.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Practice
+{
+    internal class Linq
+    {
+        static void Main()
+        {
+            int[] numbers = { 2, 5, 8, 11, 14, 17 };
+
+            var result = numbers
+                        .Where(n => n % 2 == 0)
+                        .Select(n => n * 5);
+
+            foreach (var n in result)
+            {
+                Console.WriteLine(n);
+            }
+
+        }
+    }
+}
