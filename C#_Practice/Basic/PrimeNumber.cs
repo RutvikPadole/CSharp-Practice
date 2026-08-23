a7265b38f3377af8e439df397b7a6d8d591a978b
RutvikPadole <rutvikpadole23@gmail.com>
2026-08-23 22:57:03 +0530

    Complete task

---
+++ C#_Practice/Basic/PrimeNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Practice.Basic
+{
+    internal class PrimeNumber
+    {
+        // Write a C# program to check whether a number is prime or not.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your number");
+            int num = Convert.ToInt32 (Console.ReadLine());
+
+            int count = 0;
+            
+            for(int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+
+                    count++;
+            }
+
+            if (count == 2)
+                Console.WriteLine("Prime number");
+            else
+                Console.WriteLine("Not prime number");
+
+        }
+    }
+}
+
+
