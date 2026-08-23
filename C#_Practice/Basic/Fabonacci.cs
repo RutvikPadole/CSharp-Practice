ff3a6e3d3adf8ce253c43bbde2057b3a7991045c
RutvikPadole <rutvikpadole23@gmail.com>
2026-08-23 22:55:00 +0530

    Complete task

---
+++ C#_Practice/Basic/Fabonacci.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Practice.Basic
+{
+    internal class Fabonacci
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter your number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int a = 0;
+            int b = 1;
+            int c;
+
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine(a+ " ");
+
+                c = a + b;
+                a = b;
+                b = c;
+            }
+
+        }
+    }
+}
