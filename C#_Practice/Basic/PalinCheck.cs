fbdd9558fb3f9bf1062006ef99ad17bcf6ceb8c8
RutvikPadole <rutvikpadole23@gmail.com>
2026-08-23 22:56:25 +0530

    Complete task

---
+++ C#_Practice/Basic/PalinCheck.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Practice.Basic
+{
+    internal class PalinCheck
+    {
+        // Check whether a number is palindrome
+        static void Main()
+        {
+            Console.WriteLine("Enter your Palindrome number");
+            int num = Convert.ToInt32 (Console.ReadLine());
+
+            int original = num;
+            int result = 0;
+
+            while (num > 0)
+            {
+                int digit = num % 10;
+                result = result * 10 + digit;
+                num = num / 10;
+
+            }
+            if (original == result)
+
+                Console.WriteLine("This is palindrome number");
+            else
+                Console.WriteLine("This is not palindrome number");
+        }
+    }
+}
