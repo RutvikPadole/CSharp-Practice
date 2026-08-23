44fc34bbd615462eaa03dadaa471a60e5aa27816
RutvikPadole <rutvikpadole23@gmail.com>
2026-08-23 22:56:07 +0530

    Complete task

---
+++ C#_Practice/Basic/NumReverse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Practice.Basic
+{
+    internal class NumReverse
+    {
+        static void Main()
+        {
+            //Write a C# program to reverse a number.
+
+            Console.WriteLine("Enter your number");
+            int num = Convert.ToInt32 (Console.ReadLine());
+
+            int reverse = 0;
+
+            while (num > 0)
+            {
+                int digit = num % 10;
+                reverse = reverse * 10 + digit;
+                num = num / 10;
+
+            }
+            Console.WriteLine("Reverse number is : " + reverse);
+        }
+    }
+}
