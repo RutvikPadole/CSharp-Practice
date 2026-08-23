c2c262500931de30e41a182b281e8e3d598e0059
RutvikPadole <rutvikpadole23@gmail.com>
2026-08-23 22:56:45 +0530

    Complete task

---
+++ C#_Practice/Basic/PalindromeStr.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Practice.Basic
+{
+    internal class PalindromeStr
+    {
+        static void Main()
+        {
+            string str = "madam";
+            string rev = new string (str.Reverse ().ToArray());
+
+            if (str == rev)
+                Console.WriteLine("Palindrome");
+            else
+                Console.WriteLine("Not palindrome");
+        }
+
+    }
+}
