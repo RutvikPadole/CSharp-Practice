0b2267e65d6eef6f8e6a36e551b694c2589098b3
RutvikPadole <rutvikpadole23@gmail.com>
2026-08-25 20:02:12 +0530

    Reverse array

---
+++ C#_Practice/Array/ReverseArray.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Practice.Array
+{
+    internal class ReverseArray
+    {
+        // Write a C# program to reverse this array:
+        static void Main(string[] args)
+        {
+            int[] arr = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+
+            for (int i = arr.Length -1; i>= 0; i--)
+            {
+                Console.WriteLine(arr[i] + " ");
+            }
+        }
+    }
+}
