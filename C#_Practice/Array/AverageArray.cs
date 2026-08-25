6804e0014fa81da0bbda712d52749461a6bc5943
RutvikPadole <rutvikpadole23@gmail.com>
2026-08-25 19:54:40 +0530

    complete task

---
+++ C#_Practice/Array/AverageArray.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Practice.Array
+{
+    internal class AverageArray
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 10, 20, 30, 40, 50 };
+
+
+        }
+    }
+}
