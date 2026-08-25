8a5ac27b3765139fbd7f330ce76cb2a9058d4068
RutvikPadole <rutvikpadole23@gmail.com>
2026-08-25 20:00:41 +0530

    Check positive negative number

---
+++ C#_Practice/Array/CheckPoNe.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace C__Practice.Array
+{
+    internal class CheckPoNe
+    {
+        // Print actual positive and negative number.
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 10, -5, 20, -8, 0, 15, -3 };
+
+
+            for (int i=0; i< arr.Length; i++)
+            {
+                if (arr[i] >0)
+                {
+                    Console.WriteLine(arr[i] + " is positive");
+                }
+                else if (arr[i] <0)
+                {
+                    Console.WriteLine(arr[i] + " is negative");
+                }
+                else if (arr[i] ==0)
+                {
+                    Console.WriteLine(arr[i] + " is zero");
+                }
+                
+                    
+            }
+        }
+    }
+}
---
+++ C#_Practice/Array/CountPoNe.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Practice.Array
+{
+    internal class CountPoNe
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 10, -5, 20, -8, 0, 15, -3 };
+
+            int positive = 0;
+            int negative = 0;
+            int zero = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > 0)
+                {
+                    positive++;
+                }
+                else if (arr[i] < 0)
+                {
+                    negative++;
+                }
+                else if (arr[i] == 0)
+                {
+                    zero++;
+                }
+            }
+
+            Console.WriteLine("Positive number is :- " + positive);
+            Console.WriteLine("Negative number is :- " + negative);
+            Console.WriteLine("Total zero is :- " + zero);
+
+        }
+    }
+}
