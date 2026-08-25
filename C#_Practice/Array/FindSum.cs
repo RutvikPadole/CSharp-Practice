805c320d915bcb48065f0f56223f6cb91a1ac60b
RutvikPadole <rutvikpadole23@gmail.com>
2026-08-25 20:01:55 +0530

    Find sum of number in array

---
+++ C#_Practice/Array/FindSum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Practice.Array
+{
+    internal class FindSum
+    {
+        // Find the Sum of All Elements in an Array.
+        static void Main()
+        {
+            int[] arr = { 10, 20, 30, 40, 50};
+
+            int sum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum = sum + arr[i];
+            }
+
+            Console.WriteLine("The total sum of number is :- " +  sum);
+        }
+    }
+}
