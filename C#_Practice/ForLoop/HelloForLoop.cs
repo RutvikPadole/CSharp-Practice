e5fcc7009a7a069dc4da71f972ecc6c8336d689e
RutvikPadole <rutvikpadole23@gmail.com>
2026-08-26 20:58:34 +0530

    simple hello program using for loop

---
+++ C#_Practice/ForLoop/HelloForLoop.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Practice.ForLoop
+{
+    internal class HelloForLoop
+    {
+        //Task A — Warm-up: Print "Hello" 5 times.
+       static void Main()
+        {
+            
+            for (int i=1; i<=5; i++)
+            {
+                Console.WriteLine("Hello");
+            }
+        }
+    }
+}
