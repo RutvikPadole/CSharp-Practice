935dede95dacfe27fb3b73f07954f92cfb6095d8
RutvikPadole <rutvikpadole23@gmail.com>
2026-08-26 20:40:00 +0530

    Reverse string

---
+++ C#_Practice/Basic/ReversedStr.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Practice.Basic
+{
+    internal class ReversedStr
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter string");
+            string Input = Console.ReadLine();
+
+            int length = Input.Length;
+            Console.WriteLine("Length " + length);
+
+            char[] chars = Input.ToCharArray();
+
+            string reversed = new string(chars);
+
+            Console.WriteLine("Reversed : " + reversed);
+                                        
+        }
+    }
+}
