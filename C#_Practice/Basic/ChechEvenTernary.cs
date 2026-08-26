313c822646cc180a47083f751ce93f041b883b52
RutvikPadole <rutvikpadole23@gmail.com>
2026-08-26 20:37:59 +0530

    Check even odd number using ternary

---
+++ C#_Practice/Basic/ChechEvenTernary.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+
+namespace C__Practice.Basic
+{
+    internal class CheckEvenTernary
+    {
+        static void Main()
+        {
+            // Check if a number is even or odd.
+            // using ternary operator.
+
+            Console.WriteLine("Enter number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+           string result = num % 2 == 0 ? "Even" : "Odd";
+
+            Console.WriteLine(result);
+        }
+          
+
+    }
+}
