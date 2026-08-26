d5043ae0f114e1418b2cea75ae03201a9ac4c8a4
RutvikPadole <rutvikpadole23@gmail.com>
2026-08-26 20:38:25 +0530

    Check leap year using ternary

---
+++ C#_Practice/Basic/LeapYearUsingTernary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Practice.Basic
+{
+    internal class LeapYearUsingTernary
+    {
+        static void Main(string[] args)
+        {
+            //Rewrite your leap year checker's final if-else
+            //(Leap year / Not a leap year) using a ternary operator instead.
+
+            Console.WriteLine("Enter Year");
+            int Year = Convert.ToInt32(Console.ReadLine());
+
+            string Check = (Year % 4 == 0 && (Year % 100 != 0 || Year % 400 == 0)) ? "Leap year" : "Not leap year";
+
+            Console.WriteLine(Check);
+        }
+    }
+}
