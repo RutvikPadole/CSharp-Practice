019d35e8b1964ecc06919e63ca31e872fd74def7
RutvikPadole <rutvikpadole23@gmail.com>
2026-09-06 21:00:49 +0530

    Complete the program task Average of array

--- C#_Practice/Array/AverageArray.cs
+++ C#_Practice/Array/AverageArray.cs
@@ -8,9 +8,22 @@ namespace C__Practice.Array
     {
         static void Main(string[] args)
         {
+            // Find the Average of Array Elements
+
             int[] arr = { 10, 20, 30, 40, 50 };
 
+            int avg = 0;
+            int sum = 0;
+          
 
+            for (int i = 0; i < arr.Length; i++)
+            {
+               sum = sum + arr[i];
+
+                avg = sum / arr.Length;
+            }
+
+            Console.WriteLine("Average of array element is :- " + avg);
         }
     }
 }
