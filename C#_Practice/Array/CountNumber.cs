e65456c02582c970d4d019815a320834fe6cbd0f
RutvikPadole <rutvikpadole23@gmail.com>
2026-09-06 21:01:48 +0530

    Count Numbers Greater Than Average

---
+++ C#_Practice/Array/CountNumber.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Practice.Array
+{
+    internal class CountNumber
+    {
+        static void Main(string[] args)
+        {
+            // Count Numbers Greater Than Average
+
+            int[] arr = { 10, 20, 30, 40, 50 };
+
+            int sum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum = sum + arr[i];
+
+            }
+
+            int avg = sum / arr.Length;
+
+            int count = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > avg)
+
+                count++;
+            }
+
+            Console.WriteLine("Average of numbers :- " + avg);
+            Console.WriteLine("Number greater than average :- " + count);
+
+        }
+
+
+    }
+}
