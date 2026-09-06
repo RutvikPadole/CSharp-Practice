328c8802f05138036e24bccedb71f2f18594df41
RutvikPadole <rutvikpadole23@gmail.com>
2026-09-06 21:02:17 +0530

    Find the Sum of Even Numbers in an Array

---
+++ C#_Practice/Array/FindEven.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Practice.Array
+{
+    internal class FindEven
+    {
+        static void Main(string[] args)
+        {
+            // Find the Sum of Even Numbers in an Array 
+
+            int[] arr = { 10, 15, 20, 25, 30, 35, 40 };
+
+            int sum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                if (arr[i] % 2 == 0)
+                {
+
+                    Console.WriteLine("Even number is :- " + arr[i]);
+
+                    sum = sum + arr[i];
+                }
+
+            }
+
+            Console.WriteLine("The sum of even number is :- " + sum);
+        }
+
+        }
+    }
+
