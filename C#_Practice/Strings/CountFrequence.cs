dc1caf2fdb01c16ab5b3af0d1e6ee8bd4d349ff4
RutvikPadole <rutvikpadole23@gmail.com>
2026-09-08 22:26:55 +0530

    String Task 6 — Count Frequency of a Character "G"

---
+++ C#_Practice/Strings/CountFrequence.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Practice.Strings
+{
+    internal class CountFrequence
+    {
+        static void Main(string[] args)
+        {
+            // String Task 6 — Count Frequency of a Character "G"
+
+            string str = "Programming";
+
+            int count = 0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == 'g')
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine("The Frequency of g is :- " + count);
+        }
+    }
+}
