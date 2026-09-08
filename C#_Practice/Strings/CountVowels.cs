be7de67b4966911410104be5491c0cc8a4274f6c
RutvikPadole <rutvikpadole23@gmail.com>
2026-09-08 22:28:13 +0530

    String Task 3 — Count Vowels

---
+++ C#_Practice/Strings/CountVowels.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Practice.Strings
+{
+    internal class CountVowels
+    {
+        static void Main(string[] args)
+        {
+            // String Task 3 — Count Vowels
+
+            string str = "Hello World";
+
+            int count = 0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine("Vowels :- " + count);
+
+        }
+    
+ } }
+
