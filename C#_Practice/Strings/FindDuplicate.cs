bd097e63f8c155ea03c956d33ea2d72bbc9235e7
RutvikPadole <rutvikpadole23@gmail.com>
2026-09-08 22:26:25 +0530

    String Task 7 — Find Duplicate Characters

---
+++ C#_Practice/Strings/FindDuplicate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Practice.Strings
+{
+    internal class FindDuplicate
+    {
+        static void Main(string[] args)
+        {
+            //String Task 7 — Find Duplicate Characters
+
+            string str = "Programming";
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                for (int j = i+1; j < str.Length; j++)
+                {
+                    if (str[i] == str[j])
+                    {
+                        Console.WriteLine("Duplicate Character is :- " + str[i]);
+                        break;
+                    }
+
+                }
+            }
+        }
+    }
+}
