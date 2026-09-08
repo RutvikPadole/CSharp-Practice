23cdbc8d09182d4af76d4eec6522e51655a1c9a4
RutvikPadole <rutvikpadole23@gmail.com>
2026-09-08 22:27:27 +0530

    String Task 5 — Count Words in a String

---
+++ C#_Practice/Strings/CountWord.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Practice.Strings
+{
+    internal class CountWord
+    {
+        static void Main(string[] args)
+        {
+            // String Task 5 — Count Words in a String
+
+            string str = "I am learning C Sharp language";
+
+            int count = 0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i]  == ' ')
+                {
+                    count++;
+
+                }
+            }
+
+            count++;
+            Console.WriteLine("The total number of word is :- " + count);
+
+        }
+    }
+}
