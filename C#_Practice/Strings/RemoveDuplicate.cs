127379f131d4d3c618facbabb55557aef9270768
RutvikPadole <rutvikpadole23@gmail.com>
2026-09-08 22:25:26 +0530

    String Task 8 — Remove Duplicate Characters

---
+++ C#_Practice/Strings/RemoveDuplicate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Practice.Strings
+{
+    internal class RemoveDuplicate
+    {
+        static void Main()
+        {
+            string str = "programming";
+            string result = "";
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (!result.Contains(str[i]))
+                {
+                    result = result + str[i];
+                }
+            }
+
+            Console.WriteLine("After removing duplicates: " + result);
+        }
+    }
+}
+        
