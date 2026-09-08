fc479e91be47ce866b25b508e7dc68a587f14214
RutvikPadole <rutvikpadole23@gmail.com>
2026-09-08 22:29:17 +0530

    String Task 2 — Check Palindrome

---
+++ C#_Practice/Strings/Palindrom.cs
@@ -0,0 +1,35 @@
+﻿using C__Practice.Basic;
+using C__Practice.WhileLoop;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Practice.Strings
+{
+    internal class Palindrom
+    {
+        static void Main(string[] args)
+        {
+            // String Task 2 — Check Palindrome
+
+            Console.WriteLine("Enter your string ");
+            string str = Console.ReadLine();
+
+            string Reverse = "";
+
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                Reverse = Reverse + str[i];
+            }
+            
+            if (str == Reverse)
+            {
+                Console.WriteLine("It is palindrome");
+            }
+            else
+            {
+                Console.WriteLine("It is not palindrome");
+            }
+        }
+    }
+}
