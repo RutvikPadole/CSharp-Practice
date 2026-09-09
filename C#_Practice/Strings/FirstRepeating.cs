9bd8be86dac7b1a05e1052eeee6828d9d317ba0b
RutvikPadole <rutvikpadole23@gmail.com>
2026-09-09 23:51:47 +0530

    String Task 10 — Find the First Repeating Character

---
+++ C#_Practice/Strings/FirstRepeating.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Practice.Strings
+{
+    internal class FirstRepeating
+    {
+        static void Main(string[] args)
+        {
+            string str = "Programming";
+
+            for (int i =0; i < str.Length; i++)
+            {
+                for (int j = i+1; j < str.Length; j++)
+                {
+                   if (str[i] == str[j])
+                    {
+                        Console.WriteLine("First repeating character is :- " + str[i]);
+                    }
+                    break;
+                }
+                
+            }
+        }
+    }
+}
