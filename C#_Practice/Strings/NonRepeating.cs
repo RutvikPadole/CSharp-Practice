dabc0c11397fe11ca49ef779ae6f9e730d3c14f7
RutvikPadole <rutvikpadole23@gmail.com>
2026-09-09 23:52:32 +0530

    String Task 9 — Find the First Non-Repeating Character

---
+++ C#_Practice/Strings/NonRepeating.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace C__Practice.Strings
+{
+    internal class NonRepeating
+    {
+        static void Main()
+        {
+            // String Task 9 — Find the First Non-Repeating Character
+
+            string str = "Programming";
+            int count = 0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                
+                for(int j =0; j < str.Length; j++)
+                {
+                    if (str[i] == str[j])
+                    {
+                        count++;
+                    }
+                }
+
+                if (count == 1)
+                {
+                    Console.WriteLine("First non repeating character is :- " + str[i]);
+                    break;
+                }
+
+            }
+        }
+    }
+}
